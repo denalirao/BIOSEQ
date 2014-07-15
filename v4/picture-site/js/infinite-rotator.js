a6aea3cbf9b5ffe69480fc9a5b86ecc4dd288ca3
Denali <denali.rao@tufts.edu>
2014-07-15 19:28:16 -0400

    v4

--- v4/picture-site/js/infinite-rotator.js
+++ v4/picture-site/js/infinite-rotator.js
@@ -22,6 +22,41 @@ $(window).load(function() {	//start after HTML, images have loaded
 			//cross-fade time (in milliseconds)
 			var fadeTime = 2500;
 			//count number of items
+			var numberOfItems = $('.rotating-item2').length;
+			//set current item
+			var currentItem = 0;
+			//show first item
+			$('.rotating-item2').eq(currentItem).fadeIn(initialFadeIn);
+			//loop through the items		
+			var infiniteLoop = setInterval(function(){
+				$('.rotating-item2').eq(currentItem).fadeOut(fadeTime);
+				if(currentItem == numberOfItems -1){
+					currentItem = 0;
+				}else{
+					currentItem++;
+				}
+				$('.rotating-item2').eq(currentItem).fadeIn(fadeTime);
+			}, itemInterval);	
+		}	
+	};
+	InfiniteRotator.init();
+});
+
+
+
+
+$(window).load(function() {	//start after HTML, images have loaded
+	var InfiniteRotator = 
+	{
+		init: function()
+		{
+			//initial fade-in time (in milliseconds)
+			var initialFadeIn = 1000;
+			//interval between items (in milliseconds)
+			var itemInterval = 5000;
+			//cross-fade time (in milliseconds)
+			var fadeTime = 2500;
+			//count number of items
 			var numberOfItems = $('.rotating-item').length;
 			//set current item
 			var currentItem = 0;
