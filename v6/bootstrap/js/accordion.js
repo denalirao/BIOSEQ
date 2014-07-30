fda58fe1502f36843d13c0c7cebe589b5ca7e2bc
Denali <denali.rao@tufts.edu>
2014-07-30 11:39:40 -0400

    link

--- v6/bootstrap/js/accordion.js
+++ v6/bootstrap/js/accordion.js
@@ -17,3 +17,23 @@ $(function () {
             if (active) $('#accordion .in').collapse('hide');
         });
         });
+
+$(function () {
+        var active = true;
+        $('#collapse-init2').click(function () {
+            if (active) {
+                active = false;
+                $('.panel-collapse2').collapse('show');
+                $('.panel-title2').attr('data-toggle', '');
+                $(this).text('Collapse All');
+            } else {
+                active = true;
+                $('.panel-collapse2').collapse('hide');
+                $('.panel-title2').attr('data-toggle', 'collapse');
+                $(this).text('Expand All');
+            }
+        });
+        $('#accordionTwo').on('show.bs.collapse', function () {
+            if (active) $('#accordionTwo .in').collapse('hide');
+        });
+        });
