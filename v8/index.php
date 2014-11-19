4bf59eeb7a0bb349176afc39ed942d9c95b1e4fd
Denali <denali.rao@tufts.edu>
2014-11-19 14:29:18 -0500

    v8

---
+++ v8/index.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<?include 'header.php';?>
+
+    <div class="jumbotron" style="top-padding:-50">
+          <center> <h2 style="color: #0033CC">BioSeq: Bioinformatics Inquiry through Sequencing</h2> </center>
+    </div>
+
+    <div class="container">
+        <div class="container"> <!-- top text -->
+          <p> <h3> Program Details </h3>
+            The primary objective of the BioSeq project is to provide students with a genuine research experience. Specific grant activities include 
+            <li> establishing a core sequencing facility for educational purposes at Tufts’ Medford campus,</li> 
+            <li>creating an entry level research-based bioinformatics course for high school students,</li>
+            <li> developing curricular units to describe bioinformatics concepts, </li> 
+            <li>and continuing Professor Walt’s long time focus on outreach to local K-12 students and teachers in the area of science education.</li>
+            <br>
+            Location: Pearson-Michael Chemistry Complex, Medford and participating local schools in Somerville, Medford, Malden, and Chinatown.
+
+            Anticipated outcome: The anticipated outcome of the project is that individuals who participate at all levels will have a greater understanding of and interest in bioinformatics and next generation sequencing.  Undergraduates will gain experience in the lab and high school students will get a taste of genuine contemporary research.</p>
+            <br>
+          <!--<p>
+            <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+          </p>-->
+        </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <p><h3> Interested High Schools: </h3>
+              High schools that wish to participate should have basic laboratory facilities such as blacktop benches, working sinks, gas lines and a fume hood. In the process of scheduling your project we will discuss basic equipment.  In most cases, we can provide necessary equipment.
+              <br> <br>
+              Teachers who participate in the project should be prepared to do some extra reading and some team teaching on the subjects we cover. Our aim is to have teachers comfortable enough with the project to be able to do it themselves, given the materials.
+              <h3> Interested Undergraduates: </h3>
+              Tufts students who are interested in participating should contact Project Administrator Karen O’Hagan. Students should be prepared to submit an unofficial transcript, a resume, and the name of a science professor who can be contacted as a reference.
+              <br>
+              Tufts students who participate in undergraduate research will be mentored by a graduate student and Postdocs as well as an undergraduate who has some experience in the laboratory. Faculty supervision will come in the form of lab meetings.
+              Paid summer research fellowships are available; research is performed during the semester for credit.</p>
+          </div>
+          <div class="col-md-5">
+              <div id="this-carousel-id" class="carousel slide"> <!-- class of slide for animation -->
+                <div class="carousel-inner">
+                  <div class="item active"><!-- class of active since it's the first item -->
+                    <img src="https://github.com/denalz/BIOSEQ/blob/gh-pages/v1/pictures/IMG_3755.JPG?raw=true" class = "img-responsive img-rounded" alt="" />
+                    <div class="carousel-caption">
+                      <!--<p>Caption text here</p>-->
+                    </div>
+                  </div>
+                  <div class="item">
+                    <img src="https://github.com/denalz/BIOSEQ/blob/gh-pages/v1/pictures/IMG_3777.JPG?raw=true" class="img-responsive img-rounded" alt="" />
+                  </div>
+                  <div class="item">
+                    <img src="https://github.com/denalz/BIOSEQ/blob/gh-pages/v1/pictures/IMG_3752.JPG?raw=true" class="img-responsive img-rounded" alt="" />
+                  </div>
+                  <div class="item">
+                    <img src="https://github.com/denalz/BIOSEQ/blob/gh-pages/v1/pictures/IMG_3750.JPG?raw=true" class="img-responsive img-rounded" alt="" />
+                  </div>
+                </div><!-- /.carousel-inner -->
+                <!--  Next and Previous controls below
+                      href values must reference the id for this carousel -->
+                  <a class="carousel-control left" href="#this-carousel-id" data-slide="prev">&lsaquo;</a>
+                  <a class="carousel-control right" href="#this-carousel-id" data-slide="next">&rsaquo;</a>
+              </div><!-- /.carousel -->
+              <script>
+                $(document).ready(function(){
+                $('.carousel').carousel({interval:2000});
+                });
+              </script>
+            <!--<p><a class="btn btn-default" href="#" role="button">gallery &raquo;</a></p>-->
+            </div>
+      </div>
+      <hr>
+      <div class="col-md-3">
+        <img src = "http://www.moreheadplanetarium.org/files/logo_sepa.jpg" width="80%" height = "80%"> <br>
+      </div>
+      <div class="col-md-9">
+      This project is supported by the National Institutes of Health Science Education Partnership Award. The content is solely the responsibility of the authors and does not necessarily represent the official views of the National Institutes of Health.
+    </div>
+    </div> <!-- /container -->
+
+    <hr>
+    Made with Bootstrap v3.2.0 
+    © Copyright 2011-2014 Twitter, Inc. 
+    Licensed under MIT 
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./bootstrap/js/bootstrap.js"> </script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
+    <!--for image carousel -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.7.2.min.js"><\/script>')</script>
+
+  </body>
+</html>
